0cbe70bc1faf6a210813f4abde6c15e74f30eb9d
Joaco182004 <87869138+Joaco182004@users.noreply.github.com>
2023-08-05 17:31:50 -0500

    Delete src directory

--- src/app/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-
-export const environment = {
-    production: false,
-    serverJSON: "http://localhost:8080",
-    resourceCustomer:"/api/customers",
-    resourceEmail:"/api/sendMail",
-    resourceRun:"/api/financialRun"
-};
-  
--- src/app/models/support.ts
+++
@@ -1,6 +0,0 @@
-export interface Support{
-    id?:string;
-    type:string;
-    message:string;
-    postDate:Date;
-}
